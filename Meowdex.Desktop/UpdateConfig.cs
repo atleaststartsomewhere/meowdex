0857d975c7829a342877108b912501e211a683e6
Ryan Koon <ryan@ryankoon.com>
2026-02-25 17:08:01 -0800

    add update-manifest.json as manifesturl

--- Meowdex.Desktop/UpdateConfig.cs
+++ Meowdex.Desktop/UpdateConfig.cs
@@ -3,5 +3,5 @@ namespace Meowdex.Desktop;
 public static class UpdateConfig
 {
     // Set this to a hosted JSON manifest URL (e.g. GitHub raw URL).
-    public const string ManifestUrl = "";
+    public const string ManifestUrl = "https://raw.githubusercontent.com/atleaststartsomewhere/meowdex/main/update-manifest.json";
 }
