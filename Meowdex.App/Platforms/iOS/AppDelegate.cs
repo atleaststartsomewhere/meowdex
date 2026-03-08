68aa0f45e214df77c8a8ab2f8a7841f5b7f24067
Ryan Koon <ryan@ryankoon.com>
2026-03-07 21:58:07 -0800

    removing the old maui app

--- Meowdex.App/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Meowdex.App;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
